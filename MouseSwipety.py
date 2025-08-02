4bc1cface58d76f34beedbd00b551bf0d8d2ff27
Sampreet Kishan <kishansampreet68@gmail.com>
2025-08-02 16:27:04 +0530

    Mouse movement is now displayed in the GUI itself. Colors to distinguish movement direction.

--- MouseSwipety.py
+++ MouseSwipety.py
@@ -6,47 +6,51 @@ import time
 mouse = Controller()
 last_x = mouse.position[0]
 
-def centerWindow(window): # Function to center the GUI window to the center of my screen
+def centerWindow(window):
     window.update_idletasks()
     
-    width = window.winfo_width() # Take width of GUI
-    height = window.winfo_height() # Take height of GUI
+    width = window.winfo_width() 
+    height = window.winfo_height() 
     
-    screen_width = window.winfo_screenwidth() # Take screen width
-    screen_height = window.winfo_screenheight() # Take screen height
+    screen_width = window.winfo_screenwidth() 
+    screen_height = window.winfo_screenheight() 
     
-    x = (screen_width - width) // 2 # Floor divison wrt to screen
+    x = (screen_width - width) // 2 
     y = (screen_height - height) // 2
     
-    window.geometry(f"{width}x{height}+{x}+{y}") # Convert size accordingly
+    window.geometry(f"{width}x{height}+{x}+{y}") 
     
-def buttonClick(): # Function to close the application
+def buttonClick():
     global running
     running = False
     empty_window.destroy()
     
 def track_mouse():
+    final_pos = tk.Label(empty_window, text = "No mouse movement", font=("Arial", 35), fg = "blue")
+    final_pos.place(relx = 0.5, rely = 0.5, anchor="center")
     global last_x, running
     while running:
         current_x = mouse.position[0]
         if current_x > last_x:
-            print("Moving right")
+            final_pos.config(text="Moving Right", fg = "green")
         elif current_x < last_x:
-            print("Moving left")
+            final_pos.config(text="Moving Left", fg = "red")
+        else:
+            final_pos.config(text="No mouse movement", fg = "blue")
         last_x = current_x
         time.sleep(0.01)
 
 empty_window = tk.Tk()
-empty_window.title('Testing for now') 
+empty_window.title('MouseSwipety') 
 empty_window.geometry("750x750")
 
-testing_label = tk.Label(empty_window, text = "I am simply testing for now.")
-testing_label.pack()
+testing_label = tk.Label(empty_window, text = "Mouse Action", font = ("Arial", 70))
+testing_label.place(relx=0.5, rely=0.35, anchor="center")
 
 quit_button = tk.Button(empty_window, text = "Quit", command = buttonClick, width = 20, height = 2)
 quit_button.pack(side = tk.BOTTOM, pady = 10, padx = 10, anchor = "se")
 
-centerWindow(empty_window) # Calling centerWindow function here to put it in the center
+centerWindow(empty_window)
 
 running = True
 threading.Thread(target=track_mouse, daemon=True).start()
