4e2ab797f15ee38bd680a7f64b0c96958252e669
Sampreet Kishan <kishansampreet68@gmail.com>
2025-08-09 12:57:31 +0530

    Now track Y coordinates and shows realtime coordinates.

--- MouseSwipety.py
+++ MouseSwipety.py
@@ -2,43 +2,57 @@ import tkinter as tk
 from pynput.mouse import Controller
 import threading
 import time
+import centerwindow
 
-mouse = Controller()
-last_x = mouse.position[0]
-
-def centerWindow(window):
-    window.update_idletasks()
-    
-    width = window.winfo_width() 
-    height = window.winfo_height() 
-    
-    screen_width = window.winfo_screenwidth() 
-    screen_height = window.winfo_screenheight() 
-    
-    x = (screen_width - width) // 2 
-    y = (screen_height - height) // 2
-    
-    window.geometry(f"{width}x{height}+{x}+{y}") 
-    
-def buttonClick():
+# Function to close the application when Quit button is pressed
+def buttonClick(): 
     global running
     running = False
     empty_window.destroy()
+
+# The controller function reads the mouse's current pointer position
+mouse = Controller()
+last_x = mouse.position[0]
+last_y = mouse.position[1]
+last_coor = (mouse.position[0], mouse.position[1])
     
-def track_mouse():
-    final_pos = tk.Label(empty_window, text = "No mouse movement", font=("Arial", 35), fg = "blue")
-    final_pos.place(relx = 0.5, rely = 0.5, anchor="center")
-    global last_x, running
+def trackMouse():
+    final_hor = tk.Label(empty_window, text = "No mouse movement", font = ("Arial", 35), fg = "blue")
+    final_hor.place(relx = 0.5, rely = 0.5, anchor = "center")
+    final_ver = tk.Label(empty_window, text = "No Vertical Movement", font = ("Arial", 35), fg = "blue")
+    final_ver.place(relx = 0.5, rely = 0.6, anchor = "center")
+    global last_x, last_y, running, last_coor
+    
+    coor_display = tk.Label(empty_window, text = f"{mouse.position[0]}, {mouse.position[1]}", font = ("Arial", 35), fg = "black")
+    coor_display.place(relx = 0.5, rely = 0.7, anchor = "center")
+    
     while running:
+        # Tracking X Coordinates
         current_x = mouse.position[0]
         if current_x > last_x:
-            final_pos.config(text="Moving Right", fg = "green")
+            final_hor.config(text="Moving Right", fg = "green")
         elif current_x < last_x:
-            final_pos.config(text="Moving Left", fg = "red")
+            final_hor.config(text="Moving Left", fg = "red")
         else:
-            final_pos.config(text="No mouse movement", fg = "blue")
+            final_hor.config(text="No mouse movement", fg = "blue")
         last_x = current_x
+        
+        # Tracking Y coordinates
+        current_y = mouse.position[1]
+        if current_y < last_y:
+            final_ver.config(text="Moving Up", fg="green")
+        elif current_y > last_y:
+            final_ver.config(text="Moving Down", fg="red")
+        else:
+            final_ver.config(text="No mouse movement", fg="blue")
+        last_y = current_y
+        
+        # Tracking Coordinates
+        coor_display.config(text=f"{mouse.position[0]}, {mouse.position[1]}")
+
+        
         time.sleep(0.01)
+            
 
 empty_window = tk.Tk()
 empty_window.title('MouseSwipety') 
@@ -50,9 +64,9 @@ testing_label.place(relx=0.5, rely=0.35, anchor="center")
 quit_button = tk.Button(empty_window, text = "Quit", command = buttonClick, width = 20, height = 2)
 quit_button.pack(side = tk.BOTTOM, pady = 10, padx = 10, anchor = "se")
 
-centerWindow(empty_window)
+centerwindow.centerWindow(empty_window)
 
 running = True
-threading.Thread(target=track_mouse, daemon=True).start()
+threading.Thread(target=trackMouse, daemon=True).start()
 
 empty_window.mainloop()
